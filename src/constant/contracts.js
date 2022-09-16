cd59012851d0d87e8cc3238962a365d15429c494
LoveNuna <marybarnett120@gmail.com>
2022-09-16 15:47:27 +0800

    change contract addresses because of chain redeploy

--- src/constant/contracts.js
+++ src/constant/contracts.js
@@ -1,5 +1,5 @@
 export const MarketplaceContract =
-    "human14ejqjyq8um4p3xfqj74yld5waqljf88fz25yxnma0cngspxe3les3rjp00";
+    "human1nc5tatafv6eyq7llkr2gv50ff9e22mnf70qgjlv737ktmt4eswrq36kh9m";
 
 export const CollectionCreatorContract =
-    "human1436kxs0w2es6xlqpp9rd35e3d0cjnw4sv8j3a7483sgks29jqwgs7gjky2";
+    "human1ufs3tlq4umljk0qfe8k5ya0x6hpavn897u2cnf9k0en9jr7qarqqrvjkhm";
