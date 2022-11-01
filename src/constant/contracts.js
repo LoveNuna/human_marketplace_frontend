ae6633405ed8f6a9cb1d6610fc8582207273aad6
LoveNuna <marybarnett120@gmail.com>
2022-11-01 08:21:39 +0800

    updated contract addresses

--- src/constant/contracts.js
+++ src/constant/contracts.js
@@ -1,5 +1,5 @@
 export const MarketplaceContract =
-    "human1vtjh0qqjr7m9qshqn5mfxsd6lsafhfj8pqlczsm8754pxl8fajfqlxcrqd";
+    "human10s4p4wu3e7rtdwanfkrkclvvwdpq7lmnrmc4n8g0e2wgwzm78t3qt6vlud";
 
 export const CollectionCreatorContract =
-    "human1r0k077m7xyfhy2v48x6rkfl6pr7tvcz8qm70wyy4ekd0mqz0wkuqm7ngdg";
+    "human1du2023rnwkdsv2gl53c8fw3qgtu0f3afu0v30custk9rmkaqkqhsdvrvcs";
